9cc18c464f53e67f4e099ab06c56c9e2d8f6fa49
PC839\bp-takano <fieldhawker@gmail.com>
2017-09-01 16:08:22 +0900

    hawker
    herokuデプロイへ向けてディレクトリ構成を変更

--- 006_toy/toybox/project/toybox/cybozulive/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CybozuliveConfig(AppConfig):
-    name = 'cybozulive'
--- 006_toy/toybox/project/toybox/index/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls import url
-from index import views
-
-urlpatterns = [
-    # インデックス
-    url(r'^$', views.index, name='index'),       # トップ
-    url(r'^top/$', views.index, name='index'),   # トップ
-    url(r'^index/$', views.index, name='index'), # トップ
-]
